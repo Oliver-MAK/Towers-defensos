2fdcee37f8eb55a1c2dcc07afaa343ae70056ab7
Oliver Vaňo <oliver.vano@student.tuke.sk>
2026-09-11 19:14:10 +0200

    Initial Spring boot project setup

---
+++ src/main/java/game/towerdefens/TowerDefensApplication.java
@@ -0,0 +1,13 @@
+package game.towerdefens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TowerDefensApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TowerDefensApplication.class, args);
+    }
+
+}
