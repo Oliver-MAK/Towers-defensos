2726d98c3a2d1634e5ffdf8caae596e446533c50
Oliver Vaňo <oliver.vano@student.tuke.sk>
2026-09-12 21:39:24 +0200

    2 try on MultiTargetStrategy.java

--- src/main/java/game/towerdefens/strategy/MultiTargetStrategy.java
+++ src/main/java/game/towerdefens/strategy/MultiTargetStrategy.java
@@ -5,8 +5,17 @@ import game.towerdefens.entity.Enemy;
 import java.util.List;
 
 public class MultiTargetStrategy implements TargetingStrategy {
+    private final int maxTargets;
+
+    public MultiTargetStrategy(int maxTargets){
+        this.maxTargets = maxTargets;
+    }
+
     @Override
     public List<Enemy> selectTargets(List<Enemy> enemiesInRange) {
-        return List.of();
+        if (enemiesInRange.isEmpty()){
+            return List.of();
+        }
+        return enemiesInRange.subList(0, 3);
     }
 }
