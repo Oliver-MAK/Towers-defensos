ea4fea1e586d292016c110b2511d9d511a6149a2
Oliver Vaňo <oliver.vano@student.tuke.sk>
2026-09-15 11:42:40 +0200

    slow effect SlowingTower.java

--- src/main/java/game/towerdefens/effect/SlowEffect.java
+++ src/main/java/game/towerdefens/effect/SlowEffect.java
@@ -3,6 +3,11 @@ package game.towerdefens.effect;
 import game.towerdefens.entity.Enemy;
 
 public class SlowEffect implements TowerEffect {
+    private final int durationTicks;
+
+    public SlowEffect(int durationTicks) {
+        this.durationTicks = durationTicks;
+    }
 
     @Override
     public void apply(Enemy target) {
